afb03ded0e45230624db50f70b207e2bddec12c3
wzhaoxuan <newzxuan213@gmail.com>
2024-07-21 23:31:24 +0800

    Create a Welcome Scene with Controller

--- src/main/scala/what/game/to/MainApp.scala
+++ src/main/scala/what/game/to/MainApp.scala
@@ -4,7 +4,11 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.AnchorPane
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
+import what.game.to.controller.GameSceneController
+import scalafx.Includes._
+import scalafx.stage.{Modality, Stage}
 
 object MainApp extends JFXApp {
 
@@ -23,12 +27,27 @@ object MainApp extends JFXApp {
   }
 
   def showGameScene(): Unit = {
-    val gameSceneResource = getClass.getResource("GameScene.fxml")
-    val gameSceneloader = new FXMLLoader(gameSceneResource, NoDependencyResolver)
-    gameSceneloader.load()
-    val gameSceneLayout: scalafx.scene.layout.AnchorPane = new scalafx.scene.layout.AnchorPane(gameSceneloader.getRoot[javafx.scene.layout.AnchorPane]) // Convert to ScalaFX AnchorPane
+    val gameSceneResource = getClass.getResource("/what/game/to/GameScene.fxml")
+    if (gameSceneResource == null) {
+      throw new RuntimeException("GameScene.fxml not found.")
+    }
+
+    val gameSceneLoader = new FXMLLoader(gameSceneResource, NoDependencyResolver)
+    val gameSceneLayout = gameSceneLoader.load[javafx.scene.layout.AnchorPane] // Convert to ScalaFX AnchorPane
     rootLayout.center = gameSceneLayout
+
+    val control = gameSceneLoader.getController[GameSceneController#Controller]
+    control.initialize()
+
+  }
+    def showWelcome(): Unit = {
+    val welcomeSceneResource = getClass.getResource("WelcomeScene.fxml")
+    val welcomeSceneLoader = new FXMLLoader(welcomeSceneResource, NoDependencyResolver)
+    welcomeSceneLoader.load()
+    val welcomeSceneLayout = new AnchorPane(welcomeSceneLoader.getRoot[javafx.scene.layout.AnchorPane]) // Convert to ScalaFX AnchorPane
+    rootLayout.center = welcomeSceneLayout
+    print("welcome")
   }
 
-  showGameScene()
+  showWelcome()
 }
---
+++ src/main/scala/what/game/to/controller/WelcomeController.scala
@@ -0,0 +1,14 @@
+package what.game.to.controller
+
+import scalafxml.core.macros.sfxml
+import what.game.to.MainApp
+
+@sfxml
+class WelcomeController{
+
+  def getStart(): Unit = {
+    MainApp.showGameScene()
+  }
+
+
+}
