68d0a211b2ea0c9d01961c04ac43f74fff9b4548
wzhaoxuan <newzxuan213@gmail.com>
2024-07-24 00:06:28 +0800

    Add a timeup method to stop the game

--- src/main/scala/what/game/to/model/Timer.scala
+++ src/main/scala/what/game/to/model/Timer.scala
@@ -8,28 +8,45 @@ import scalafx.util.Duration
 
 class Timer(totalTime: Int, timerLabel: Label, spawnZombieTime: Int, createZombies: () => Unit) {
   private var remainingTime = totalTime
+  private var gameRunning = true // Flag to check if the game is running
+  private var timeLine: Timeline = _
+  private var spawnTimeLine: Timeline = _
 
   def start(): Unit = {
-    val timeline = new Timeline {
+    timeLine= new Timeline {
       cycleCount = Timeline.Indefinite
       keyFrames = Seq(
         KeyFrame(Duration(700), onFinished = _ => {
-          remainingTime -= 1
-          timerLabel.text = remainingTime.toString
+          if (gameRunning) {
+            remainingTime -= 1
+            timerLabel.text = remainingTime.toString
+            if (remainingTime <= 0) {
+              timeUp()
+            }
+          }
         })
       )
+}
+      spawnTimeLine = new Timeline {
+        cycleCount = Timeline.Indefinite
+        keyFrames = Seq(
+          KeyFrame(Duration(spawnZombieTime * 700), onFinished = _ => {
+            if (gameRunning) {
+              createZombies()
+            }
+          })
+        )
+      }
+
+      timeLine.play()
+      spawnTimeLine.play()
     }
 
-    val spawnTimeLine = new Timeline{
-      cycleCount = Timeline.Indefinite
-      keyFrames = Seq(
-        KeyFrame(Duration(spawnZombieTime * 700), onFinished = _ => {
-          createZombies()
-        })
-      )
-      println(keyFrames)
-    }
-    timeline.play()
-    spawnTimeLine.play()
+      private def timeUp() = {
+        gameRunning = false
+        // Optional: You can stop the timelines here if needed
+        timeLine.stop()
+        spawnTimeLine.stop()
+
   }
 }
